739afc44670add2ab4806fb1fbb459302b0fcd55
Cameron MacFarland <distantcam@gmail.com>
2021-11-16 22:58:56 +0800

    Created builder exe

--- Source/MDK/Properties/AssemblyInfo.cs
+++ Source/MDK/Properties/AssemblyInfo.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MDK/SE")]
@@ -13,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(true)]
 [assembly: CLSCompliant(false)]
@@ -22,12 +23,14 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
+
+[assembly: InternalsVisibleTo("MDKBuilder, PublicKey=0024000004800000940000000602000000240000525341310004000001000100bd8b74ae0f14ce22ef16dd50e3ee4bab1af0a387a91a56a524d2b1baf534201eb6fba81a43218609c74240bded82e603e887216d81a774f17ea2f621d035ad2233c7f8ec9fdb30cd124288623c3881df58f901ba664cf592272f97f3807e30c6ada6040589134cbd22886fee71ad1903691eea423b5653449106a01b514728b1")]
---
+++ Source/MDKBuilder/Program.cs
@@ -0,0 +1,164 @@
+﻿using Malware.MDKServices;
+using MDK.Build;
+using MDK.Build.Composers;
+using MDK.Build.Composers.Default;
+using MDK.Build.Composers.Minifying;
+using MDK.Build.Solution;
+using MDK.Build.TypeTrimming;
+using MDK.Resources;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MDKBuilder
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            MSBuildLocator.RegisterDefaults();
+
+            var rootCommand = new RootCommand
+            {
+                new Argument<IEnumerable<string>>("files")
+            };
+
+            rootCommand.Handler = CommandHandler.Create((IEnumerable<string> files) => Process(files));
+
+            await rootCommand.InvokeAsync(args);
+        }
+
+        static async Task Process(IEnumerable<string> files)
+        {
+            var workspace = MSBuildWorkspace.Create();
+            foreach (var file in files)
+            {
+                var project = await workspace.OpenProjectAsync(file);
+
+                var builtScript = await BuildAsync(project);
+            }
+        }
+
+        static async Task<MDKProjectProperties> BuildAsync(Project project)
+        {
+            var config = LoadConfig(project);
+            if (!config.IsValid)
+                return null;
+
+            var composition = await ComposeDocumentAsync(project, config);
+
+            if (config.Options.TrimTypes)
+            {
+                var processor = new TypeTrimmer();
+                composition = await processor.ProcessAsync(composition, config);
+            }
+            ScriptComposer composer;
+            switch (config.Options.MinifyLevel)
+            {
+                case MinifyLevel.Full: composer = new MinifyingComposer(); break;
+                case MinifyLevel.StripComments: composer = new StripCommentsComposer(); break;
+                case MinifyLevel.Lite: composer = new LiteComposer(); break;
+                default: composer = new DefaultComposer(); break;
+            }
+            var script = await ComposeScriptAsync(composition, composer, config).ConfigureAwait(false);
+
+            if (composition.Readme != null)
+            {
+                script = composition.Readme + script;
+            }
+
+            WriteScript(project, config.Paths.OutputPath, script);
+            return config;
+        }
+
+        static MDKProjectProperties LoadConfig(Project project)
+        {
+            try
+            {
+                return MDKProjectProperties.Load(project.FilePath, project.Name);
+            }
+            catch (Exception e)
+            {
+                throw new BuildException(string.Format(Text.BuildModule_LoadConfig_Error, project.FilePath), e);
+            }
+        }
+
+        static async Task<ProgramComposition> ComposeDocumentAsync(Project project, MDKProjectProperties config)
+        {
+            try
+            {
+                var documentComposer = new ProgramDocumentComposer();
+                return await documentComposer.ComposeAsync(project, config).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                throw new BuildException(string.Format(Text.BuildModule_LoadContent_Error, project.FilePath), e);
+            }
+        }
+
+        static async Task<string> ComposeScriptAsync(ProgramComposition composition, ScriptComposer composer, MDKProjectProperties config)
+        {
+            try
+            {
+                var script = await composer.GenerateAsync(composition, config).ConfigureAwait(false);
+                return script;
+            }
+            catch (Exception e)
+            {
+                throw new BuildException(string.Format(Text.BuildModule_GenerateScript_ErrorGeneratingScript, composition.Document.Project.FilePath), e);
+            }
+        }
+
+        static void WriteScript(Project project, string output, string script)
+        {
+            try
+            {
+                var outputInfo = new DirectoryInfo(ExpandMacros(project, Path.Combine(output, project.Name)));
+                if (!outputInfo.Exists)
+                    outputInfo.Create();
+                File.WriteAllText(Path.Combine(outputInfo.FullName, "script.cs"), script.Replace("\r\n", "\n"), Encoding.UTF8);
+
+                var thumbFile = new FileInfo(Path.Combine(Path.GetDirectoryName(project.FilePath) ?? ".", "thumb.png"));
+                if (thumbFile.Exists)
+                    thumbFile.CopyTo(Path.Combine(outputInfo.FullName, "thumb.png"), true);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                throw new UnauthorizedAccessException(string.Format(Text.BuildModule_WriteScript_UnauthorizedAccess, project.FilePath), e);
+            }
+            catch (Exception e)
+            {
+                throw new BuildException(string.Format(Text.BuildModule_WriteScript_Error, project.FilePath), e);
+            }
+        }
+
+        static string ExpandMacros(Project project, string input)
+        {
+            var replacements = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase)
+            {
+                ["$(projectname)"] = project.Name ?? ""
+            };
+            foreach (DictionaryEntry envVar in Environment.GetEnvironmentVariables())
+                replacements[$"%{envVar.Key}%"] = (string)envVar.Value;
+            return Regex.Replace(input, @"\$\(ProjectName\)|%[^%]+%", match =>
+            {
+                if (replacements.TryGetValue(match.Value, out var value))
+                {
+                    return value;
+                }
+
+                return match.Value;
+            }, RegexOptions.IgnoreCase);
+        }
+    }
+}
